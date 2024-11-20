e95c41deea9a26f935831380349c97baff520bbc
Rakshitha Kailasa <kailasarakshitha@gmail.com>
2024-11-20 18:26:41 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+const port=300;
+app.get('/',(req,res)=>{
+    res.send("<h1>hello world</h1>");
+});
+app.listen(port,()=>
+{
+    console.log(`server is running on ${port}`);
+})
